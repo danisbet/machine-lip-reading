09bb9a6d50425fa9833a1e9cc17f846e2b8d67bc
alex <alex.vlissidis@gmail.com>
2018-04-24 18:38:08 -0700

    labels to text

--- preprocessing/data.py
+++ preprocessing/data.py
@@ -18,6 +18,15 @@ def text_to_labels(text):
             ret.append(26)
     return ret
 
+def labels_to_text(labels):
+# 26 is space, 27 is CTC blank char
+    text = ''
+    for c in labels:
+        if c >= 0 and c < 26:
+            text += chr(c + ord('a'))
+        elif c == 26:
+            text += ' '
+    return text
 
 def load_data(datapath, verbose=False, num_samples=-1, ctc_encoding=False):
     oh = OneHotEncoder()
