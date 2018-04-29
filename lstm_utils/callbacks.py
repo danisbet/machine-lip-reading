5d805f542ade4b8703bca7b9f2c9f77298bb5c74
QingyangZhao <276676925@qq.com>
2018-04-29 05:24:09 -0700

    num+proc

--- lstm_utils/callbacks.py
+++ lstm_utils/callbacks.py
@@ -75,12 +75,11 @@ class Statistics(keras.callbacks.Callback):
         source_str = []
 
         while num_left > 0:
-            num_proc = min(x_train.shape[0], num_left)
+            num_proc = min(self.x_train.shape[0], num_left)
             input_data = {'the_input': self.x_train[0:num_proc], 'the_labels': self.y_train[0:num_proc],
-             'label_length': label_len_train[0:num_proc], 'input_length': input_len_train[0:num_proc]}
+             'label_length': self.label_len_train[0:num_proc], 'input_length': self.input_len_train[0:num_proc]}
             y_pred = K.function([input_data, K.learning_phase()], [0])
-            input_length    = self.input_length[0:num_proc]
-            decoded_res     = decode(y_pred, input_length)
+            decoded_res     = decode(y_pred, input_data['input_length'])
             for i in range(0, num_proc):
                 source_str.append(labels_to_text(self.y_train[i]))
             for j in range(0, num_proc):
