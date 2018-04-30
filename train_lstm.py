cbef665af6ba1d9b20b31f0ef869b67259a357b7
QingyangZhao <276676925@qq.com>
2018-04-30 11:14:32 -0700

    5000 words

--- train_lstm.py
+++ train_lstm.py
@@ -220,7 +220,7 @@ def main():
     if start_epoch >= epochs:
         print "start_epoch too large, should be smaller than 2000!"
 
-    max_seq_len = 20
+    max_seq_len = 25
     x_s = np.ndarray(shape=(0, max_seq_len, 50, 100, 3))
     y_s = np.ndarray(shape=(0, 6))
     label_lens = np.array([])
