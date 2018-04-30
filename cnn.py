16447aa42c248ea9507c1ee2f48041bf88326c82
Daniel Avery Nisbet <danisbet95@gmail.com>
2018-04-30 06:10:30 +0000

    added X and y data files to gitignore and more filters to double headed architecture

--- cnn.py
+++ cnn.py
@@ -22,17 +22,17 @@ class Cnn(object):
         
         """
         self.head1_zero1 = ZeroPadding3D(padding=(1,2,2), name='head1_zero1')(self.input_data)
-        self.head1_conv1 = Conv3D(16, (3,5,5), strides=(1,2,2), activation='relu', kernel_initializer='glorot_normal', name='head1_conv1')(self.head1_zero1)
+        self.head1_conv1 = Conv3D(32, (3,5,5), strides=(1,2,2), activation='relu', kernel_initializer='glorot_normal', name='head1_conv1')(self.head1_zero1)
         self.head1_maxp1 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), name='head1_maxp1')(self.head1_conv1)
         self.head1_drop1 = Dropout(0.5)(self.head1_maxp1)
 
         self.head1_zero2 = ZeroPadding3D(padding=(1,2,2), name='head1_zero2')(self.head1_drop1)
-        self.head1_conv2 = Conv3D(32, (3,5,5), strides=(1,1,1), activation='relu', kernel_initializer='glorot_normal', name='head1_conv2')(self.head1_zero2)
+        self.head1_conv2 = Conv3D(64, (3,5,5), strides=(1,1,1), activation='relu', kernel_initializer='glorot_normal', name='head1_conv2')(self.head1_zero2)
         self.head1_maxp2 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), name='head1_maxp2')(self.head1_conv2)
         self.head1_drop2 = Dropout(0.5)(self.head1_maxp2)
 
         self.head1_zero3 = ZeroPadding3D(padding=(1,1,1), name='head1_zero3')(self.head1_drop2)
-        self.head1_conv3 = Conv3D(64, (3,3,3), strides=(1,1,1), activation='relu', kernel_initializer='glorot_normal', name='head1_conv3')(self.head1_zero3)
+        self.head1_conv3 = Conv3D(96, (3,3,3), strides=(1,1,1), activation='relu', kernel_initializer='glorot_normal', name='head1_conv3')(self.head1_zero3)
         self.head1_maxp3 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), name='head1_maxp3')(self.head1_conv3)
         self.head1_drop3 = Dropout(0.5)(self.head1_maxp3)
 
@@ -44,23 +44,23 @@ class Cnn(object):
         # Head Two: Consider larger temporal features, 
 
         # """
-        # self.head2_zero1 = ZeroPadding3D(padding=(3,2,2), name='head2_zero1')(self.input_data)
-        # self.head2_conv1 = Conv3D(4, (7,5,5), strides=(1,2,2), activation='relu', kernel_initializer='glorot_normal', name='head2_conv1')(self.head2_zero1)
-        # self.head2_maxp1 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), name='head2_maxp1')(self.head2_conv1)
-        # self.head2_drop1 = Dropout(0.5)(self.head2_maxp1)
+        self.head2_zero1 = ZeroPadding3D(padding=(3,2,2), name='head2_zero1')(self.input_data)
+        self.head2_conv1 = Conv3D(16, (7,5,5), strides=(1,2,2), activation='relu', kernel_initializer='glorot_normal', name='head2_conv1')(self.head2_zero1)
+        self.head2_maxp1 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), name='head2_maxp1')(self.head2_conv1)
+        self.head2_drop1 = Dropout(0.5)(self.head2_maxp1)
 
-        # self.head2_zero2 = ZeroPadding3D(padding=(3,2,2), name='head2_zero2')(self.head2_drop1)
-        # self.head2_conv2 = Conv3D(16, (7,5,5), strides=(1,1,1), activation='relu', kernel_initializer='glorot_normal', name='head2_conv2')(self.head2_zero2)
-        # self.head2_maxp2 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), name='head2_maxp2')(self.head2_conv2)
-        # self.head2_drop2 = Dropout(0.5)(self.head2_maxp2)
+        self.head2_zero2 = ZeroPadding3D(padding=(3,2,2), name='head2_zero2')(self.head2_drop1)
+        self.head2_conv2 = Conv3D(32, (7,5,5), strides=(1,1,1), activation='relu', kernel_initializer='glorot_normal', name='head2_conv2')(self.head2_zero2)
+        self.head2_maxp2 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), name='head2_maxp2')(self.head2_conv2)
+        self.head2_drop2 = Dropout(0.5)(self.head2_maxp2)
 
-        # self.head2_zero3 = ZeroPadding3D(padding=(3,1,1), name='head2_zero3')(self.head2_drop2)
-        # self.head2_conv3 = Conv3D(32, (7,3,3), strides=(1,1,1), activation='relu', kernel_initializer='glorot_normal', name='head2_conv3')(self.head2_zero3)
-        # self.head2_maxp3 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), name='head2_maxp3')(self.head2_conv3)
-        # self.head2_drop3 = Dropout(0.5)(self.head2_maxp3)
+        self.head2_zero3 = ZeroPadding3D(padding=(3,1,1), name='head2_zero3')(self.head2_drop2)
+        self.head2_conv3 = Conv3D(64, (7,3,3), strides=(1,1,1), activation='relu', kernel_initializer='glorot_normal', name='head2_conv3')(self.head2_zero3)
+        self.head2_maxp3 = MaxPooling3D(pool_size=(1,2,2), strides=(1,2,2), name='head2_maxp3')(self.head2_conv3)
+        self.head2_drop3 = Dropout(0.5)(self.head2_maxp3)
 
-        # self.head2_flat  = Flatten()(self.head1_drop3)
-        # print('head2 finished')
+        self.head2_flat  = Flatten()(self.head1_drop3)
+        print('head2 finished')
 
 
         # """
@@ -68,17 +68,17 @@ class Cnn(object):
         # """
 
 
-        # self.concat = concatenate([self.head1_flat, self.head2_flat], axis=1)
+        self.concat = concatenate([self.head1_flat, self.head2_flat], axis=1)
         # temp_model = Model(input=self.input_data, output=self.concat)
         # temp_model.summary()
-        # # self.flat = Flatten()(self.concat)
-        self.predictions = Dense(self.output_size, activation='softmax', kernel_initializer='glorot_normal', name='dense1')(self.head1_flat)
+         # self.flat = Flatten()(self.concat)
+        self.predictions = Dense(self.output_size, activation='softmax', kernel_initializer='glorot_normal', name='dense1')(self.concat)
         
         model = Model(input=self.input_data, output=self.predictions)
         
         model.summary()
         
-        adam = optimizers.Adam(lr=.001, decay=.001)
+        adam = optimizers.Adam(lr=.000008, decay=.005)
         model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
         print("model built")
 
