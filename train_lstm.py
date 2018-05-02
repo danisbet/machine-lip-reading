63506af514ef0c2a3b7980ac12cc0ee0cf6d7a09
QingyangZhao <276676925@qq.com>
2018-05-01 19:54:18 -0700

    add print labels shape

--- train_lstm.py
+++ train_lstm.py
@@ -42,10 +42,11 @@ def ctc_lambda_func(args):
     label_length = K.cast(tf.squeeze(label_length),'int32')
     input_length = K.cast(tf.squeeze(input_length),'int32')
     labels = K.ctc_label_dense_to_sparse(labels, label_length)
+    print("labels shape", labels.shape)
     return tf.nn.ctc_loss(labels, y_pred, input_length, ctc_merge_repeated=False,
                          ignore_longer_outputs_than_inputs=True, time_major=True)
-    y_pred = y_pred[:, :, :]
-    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
+    # y_pred = y_pred[:, :, :]
+    # return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
 
 
