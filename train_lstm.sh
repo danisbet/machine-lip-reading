477c346afc6edf77a668f6c3b5318fb077f80237
alexvlis <alex.vlissidis@gmail.com>
2018-05-10 12:24:37 -0700

    latest model + early stopping

--- train_lstm.sh
+++ train_lstm.sh
@@ -9,7 +9,7 @@
 #SBATCH --time=23:59:00
 #
 # Partition:
-#SBATCH --partition=savio2_1080ti
+#SBATCH --partition=savio2_bigmem
 #
 # Number of nodes:
 #SBATCH --nodes=1
@@ -17,14 +17,11 @@
 # Number of tasks (one for each GPU desired for use case) (example):
 #SBATCH --ntasks=1
 #
-# Processors per task (please always specify the total number of processors twice the number of GPUs):
+##SBATCH --gres=gpu:1
+#
 #SBATCH --cpus-per-task=2
 #
-# Memory:
-#SBATCH --mem-per-cpu=31G
-#
-#Number of GPUs, this can be in the format of "gpu:[1-4]", or "gpu:K80:[1-4] with the type included
-#SBATCH --gres=gpu:1
+##SBATCH --mem-per-cpu=31G
 #
 # Mail type:
 #SBATCH --mail-type=all
@@ -35,7 +32,9 @@
 ## Command(s) to run:
 source deactivate
 module purge
-module load tensorflow/1.5.0-py35-pip-gpu
-module unload cudnn/7.1
-module load cudnn/7.0.5
-python train_lstm_oh.py
+#module load tensorflow/1.5.0-py35-pip-gpu
+#module unload cudnn/7.1
+#module load cudnn/7.0.5
+module load python/2.7
+source activate cs294-129
+python train_lstm_oh.py -sid 19
--- train_lstm_oh.py
+++ train_lstm_oh.py
@@ -1,4 +1,5 @@
 from lstm_utils.callbacks import Statistics
+from preprocessing.data_lstm import read_data_for_speaker
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
@@ -22,11 +23,12 @@ from keras import backend as K
 from keras.models import Model
 from keras.applications.vgg16 import VGG16
 from keras import regularizers
+from keras.callbacks import EarlyStopping
 
 from sklearn.model_selection import train_test_split
 
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
-DATA_PATH = "/global/scratch/alex_vlissidis"
+DATA_PATH = "/global/scratch/alex_vlissidis/lipreading_data"
 
 
 def build_model(input_size, output_size=28):
@@ -43,42 +45,37 @@ def build_model(input_size, output_size=28):
     #     input_size = (self.frames_n, self.img_w, self.img_h, self.img_c)
 
     # self.input_data = Input(name='the_input', shape=input_size, dtype='float32')
-    input_data = Input(name='the_input', shape=input_size, dtype='float32')
+    input_data = Input(name='input', shape=input_size, dtype='float32')
     
     ## padding used on the height and width before convolving
     #  shape:(None, 20, 54, 104, 3)
     model = Sequential()
-    model.add(ZeroPadding3D(padding=(0, 2, 2),input_shape = (input_size), name='padding1'))
+    model.add(ZeroPadding3D(padding=(0, 2, 2),input_shape=(input_size), name='padding1', data_format='channels_last'))
+    #model.add(TimeDistributed(Conv2D(filters=34, kernel_size=(3, 3), kernel_initializer='he_normal', strides=(2, 2), padding='same', activation='relu')))
 
-    ## 2D Convolution on each time sequence, relu activation
-    #  shape 1st conv: (None, 20, 27, 52, 32)
-    #  shape 2nd conv: (None, 20, 14, 26, 32)
-    model.add(TimeDistributed(Conv2D(filters=34, kernel_size=(3, 3), kernel_initializer='he_normal', strides=(2, 2), padding='same', activation='relu')))
+    model.add(TimeDistributed(Conv2D(filters=42, kernel_size=(3, 3), kernel_initializer='he_normal', strides=(2, 2), padding='same', activation='relu')))
     model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='max1')))
 
-    model.add(TimeDistributed(Conv2D(filters=64, kernel_size=(3, 3), kernel_initializer='he_normal', strides=(2, 2), padding='same', activation='relu')))
+    model.add(TimeDistributed(Conv2D(filters=96, kernel_size=(3, 3), kernel_initializer='he_normal', strides=(1, 1), padding='same', activation='relu')))
     model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='max2')))
-    model.add(Dropout(0))
 
-    ## 2D Convolution on each time sequence, relu activation
-    #  shape 1st conv: (None, 20, 4, 7, 4)
-    model.add(TimeDistributed(Conv2D(filters=96, kernel_size=(3, 3), kernel_initializer='he_normal', strides=(2, 2), padding='same', activation='relu')))
+    model.add(TimeDistributed(Conv2D(filters=128, kernel_size=(3, 3), kernel_initializer='he_normal', strides=(2, 2), padding='same', activation='relu')))
+    #model.add(TimeDistributed(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='max2')))
 
     ## Flatten to gru
     #  shape: (None, 20, 112)
     model.add(TimeDistributed(Flatten()))
-    model.add(Dropout(0.0))
+
     ## Bidirectional gru
     #  shape: (None, 20, 512)
     # x_lstm = LSTM(256, return_sequences=True, kernel_initializer='Orthogonal', name='lstm1')(input_lstm)
     # x_lstm = LSTM(256, return_sequences=True, kernel_initializer='Orthogonal', name='lstm2')(x_lstm)
-    model.add(Bidirectional(GRU(256, return_sequences=True, kernel_initializer='Orthogonal', name='gru1'),
-                           merge_mode='concat'))
-    model.add(Bidirectional(GRU(256, return_sequences=True, kernel_initializer='Orthogonal', name='gru2'),
-                           merge_mode='concat'))
+    model.add(Bidirectional(GRU(256, return_sequences=True, kernel_initializer='Orthogonal', name='gru1'), merge_mode='concat'))
+    model.add(Bidirectional(GRU(256, return_sequences=True, kernel_initializer='Orthogonal', name='gru2'), merge_mode='concat'))
     
     model.add(Flatten())
-    model.add(Dense(1024, activation='relu', kernel_initializer='he_normal', name='fc1', kernel_regularizer=regularizers.l2(0.0)))
+    model.add(Dense(4096, activation='relu', kernel_initializer='he_normal', name='fc1', kernel_regularizer=regularizers.l2(0.01)))
+    model.add(Dropout(0.7))
     model.add(Dense(output_size, kernel_initializer='he_normal', name='dense2'))
 
     ## prepare input for ctc loss
@@ -89,26 +86,29 @@ def build_model(input_size, output_size=28):
     return model
 
 def build_model_vgg16(input_size, output_size=28):
-    input_data = Input(name='the_input', shape=input_size, dtype='float32')
+    K.set_image_data_format('channels_last')
+    input_data = Input(name='input', shape=input_size, dtype='float32')
 
+    # VGG16
     vgg16 = VGG16(weights='imagenet', include_top=False, input_shape=input_size[1:], pooling='max')
     x = TimeDistributed(vgg16)(input_data)
     x = TimeDistributed(Flatten())(x)
 
-    # let's add a fully-connected layer
+    # Bi-GRU
     x = Bidirectional(GRU(256, return_sequences=True, name='gru1'), merge_mode='concat')(x)
     x = Bidirectional(GRU(256, return_sequences=True, name='gru1'), merge_mode='concat')(x)
     x = Flatten()(x)
 
+    # FC + softmax
     x = Dense(4096, activation='relu', kernel_regularizer=regularizers.l2(0.01))(x)
     x = Dropout(0.2)(x)
     predictions = Dense(output_size, activation='softmax')(x)
 
     # this is the model we will train
     model = Model(inputs=input_data, outputs=predictions)
+    model.summary()
 
-    # first: train only the top layers (which were randomly initialized)
-    # i.e. freeze all convolutional InceptionV3 layers
+    # freeze all convolutional VGG16 layers
     for layer in vgg16.layers:
        layer.trainable = False
 
@@ -123,7 +123,7 @@ def pad_labels(labels, max_string_len=10):
     return np.concatenate((labels, padding), axis=1)
 
 
-def train(model, x_train, y_train, batch_size=256, epochs=100, val_train_ratio=0.2, start_epoch=0):
+def train(model, x_train, y_train, batch_size=128, epochs=100, val_train_ratio=0.2, start_epoch=0):
     ##
     # Train model, typically will train for each speaker
     ## padding the labels
@@ -131,7 +131,9 @@ def train(model, x_train, y_train, batch_size=256, epochs=100, val_train_ratio=0
     # if y_train.shape[1] != max_string_len:
     #     y_train = pad_labels(y_train, max_string_len)
 
-    adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+    early_stopping = EarlyStopping(monitor='val_acc', min_delta=0.005, patience=10, verbose=1, mode='max')
+
+    adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
     model.compile(loss='categorical_crossentropy', optimizer=adam, metrics=['accuracy'])
 
     if start_epoch > 0:
@@ -147,13 +149,14 @@ def train(model, x_train, y_train, batch_size=256, epochs=100, val_train_ratio=0
                         validation_split=val_train_ratio,
                         shuffle=True,
                         initial_epoch=start_epoch,
-                        verbose=1)
+                        verbose=1,
+                        callbacks=[early_stopping])
     return history
 
 
 def pad_input(x, max_str_len):
     #################
-    # pad on axis = 1
+    # pad on axis = 
     #################
     # sil_image = get_sil_image()
     # sil_image = [sil_image for _ in range(max_str_len - x.shape[1])]
@@ -176,30 +179,23 @@ def main():
     speaker_id = args.speaker_id
     speaker_name = 's' + str(speaker_id)
 
-    epochs = 20
+    epochs = 100
     if start_epoch >= epochs:
         print("start_epoch too large, should be smaller than 2000!")
 
     max_seq_len = 40
-    # x_s = np.ndarray(shape=(0, max_seq_len, 50, 100, 3))
-    # y_s = np.ndarray(shape=(0, 6))
-    # label_lens = np.array([])
-    # input_lens = np.array([])
-
+   
+    
     start = time.time()
-    
-    x_path = DATA_PATH + '/X.npz'
-    y_path = DATA_PATH + '/y.npz'
-    x_s = np.load(x_path)['x']
-    y_s = np.load(y_path)['y']
-    #x_s = pad_input(x_s, max_seq_len)
-    
+   
+    x = np.load('/global/scratch/alex_vlissidis/lipreading_data/s' + str(speaker_id) + '/X.npz')['x']
+    y = np.load('/global/scratch/alex_vlissidis/lipreading_data/s' + str(speaker_id) + '/y.npz')['y']
+    print(x.shape, y.shape)
+    y = y.astype(int)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True)
+ 
     end = time.time()
-    print("load data took", end - start)
-
-    print(x_s.shape, y_s.shape)
-    y_s = y_s.astype(int)
-    x_train, x_test, y_train, y_test = train_test_split(x_s, y_s, test_size=0.2, shuffle=True)
+    print("load data took", end-start)
 
     # 28 is outout size
     # run_name = datetime.datetime.now().strftime('%Y:%m:%d:%H:%M:%S')
@@ -207,23 +203,25 @@ def main():
     model = build_model_vgg16(x_train.shape[1:], y_train.shape[1])
 
     # input_len_train = np.ones((x_train.shape[0],1),dtype = np.int32)*max_seq_len
-    history = train(model, x_train, y_train, batch_size=256, epochs=epochs,
-                    start_epoch=start_epoch)
+    history = train(model, x_train, y_train, batch_size=256, epochs=epochs, start_epoch=start_epoch)
 
     print("Finish Training...")
-    model.save('models/model_lstm_oh.h5')
+    model.save('/global/scratch/alex_vlissidis/lipreading_models/model_gru-fc4096-lr0.0001-dr0.2-'+ speaker_name + '.h5')
 
     print("Plotting...")
+    nepochs = len(history.history['acc'])
     f, (ax1, ax2) = plt.subplots(2, 1)
-    ax1.plot(range(1, epochs+1), history.history['val_acc'], 'tab:blue', label="validation accuracy")
-    ax1.plot(range(1, epochs+1), history.history['acc'], 'tab:red', label="training accuracy")
+    ax1.plot(range(1, nepochs+1), history.history['val_acc'], 'tab:blue', label="validation accuracy")
+    ax1.plot(range(1, nepochs+1), history.history['acc'], 'tab:red', label="training accuracy")
 
-    ax2.plot(range(1, epochs+1), history.history['loss'], 'tab:orange', label="loss")
-    ax2.plot(range(1, epochs+1), history.history['val_loss'], 'tab:green', label="validation loss")
+    ax2.plot(range(1, nepochs+1), history.history['loss'], 'tab:orange', label="loss")
+    ax2.plot(range(1, nepochs+1), history.history['val_loss'], 'tab:green', label="validation loss")
 
     ax1.legend()
     ax2.legend()
-    f.savefig('figures/training_lstm.png', dpi=300)
+    f.savefig('figures/training_vgg16-gru-fc4096-lr0.0001-' + speaker_name + '.png', dpi=300)
+
+    print("Testing:", model.evaluate(x=x_test, y=y_test, batch_size=32))
     print("Done.")
 
 if __name__ == "__main__":
